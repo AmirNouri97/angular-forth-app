75d354d26f10fa99e3c2a6ec0a8acebf8c933a35
amirnouri97 <nouri97dev@gmail.com>
2025-09-25 20:10:15 +0330

    feat:convert the signal to Observable use toObservable function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -8,13 +9,14 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit{
   clickCount = signal(0)
+  clickCount$ = toObservable(this.clickCount)
   private destroyRef = inject(DestroyRef);
-  constructor(){
-    effect(()=>{
-      console.log(`you click ${this.clickCount()} times`);
+  // constructor(){
+  //   effect(()=>{
+  //     console.log(`you click ${this.clickCount()} times`);
       
-    })
-  }
+  //   })
+  // }
 ngOnInit(): void {
 //   const subscription = interval(1000).pipe(
 //     map((val)=>val*2))
@@ -24,6 +26,13 @@ ngOnInit(): void {
 // this.destroyRef.onDestroy(()=>{
 //   subscription.unsubscribe();
 // })
+const subscription = this.clickCount$.subscribe({
+  // next: (val)=>console.log(`you click ${this.clickCount()} ${val} times`)
+  next: (val)=>console.log(`you click ${val} times`)
+})
+this.destroyRef.onDestroy(()=>{
+  subscription.unsubscribe()
+})
 }
 onClick(){
   this.clickCount.update(prev=>prev+1)
