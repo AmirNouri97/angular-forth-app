3dac38ca0bcef300db93628bb96037df34fc2491
amirnouri97 <nouri97dev@gmail.com>
2025-09-25 19:41:33 +0330

    feat:create a signal to show the changed number when click on btn and a effect fn to react to every change of signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +7,26 @@ import { interval } from 'rxjs';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit{
+  clickCount = signal(0)
   private destroyRef = inject(DestroyRef);
+  constructor(){
+    effect(()=>{
+      console.log(`you click ${this.clickCount()} times`);
+      
+    })
+  }
 ngOnInit(): void {
-  const subscription = interval(1000).subscribe({
-    next:(value)=>console.log(value)
-  });
-this.destroyRef.onDestroy(()=>{
-  subscription.unsubscribe();
-})
+//   const subscription = interval(1000).pipe(
+//     map((val)=>val*2))
+//     .subscribe({
+//     next:(value)=>console.log(value)
+//   });
+// this.destroyRef.onDestroy(()=>{
+//   subscription.unsubscribe();
+// })
 }
+onClick(){
+  this.clickCount.update(prev=>prev+1)
+}
+
 }
